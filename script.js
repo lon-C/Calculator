39a3cb584cf33e6e2a9c9d597f39bb3dca0f7b7e
Ion <ionelu1998@gmail.com>
2025-11-11 19:18:31 +0000

    Created the function operate that takes 2 numbers and the operator and returns the result

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-function add(num1, num2) {
+let add = function add(num1, num2) {
     return num1 + num2;
 };
 
-function subtract(num1, num2) {
+let subtract = function subtract(num1, num2) {
     return num1 - num2;
 };
 
-function multiply(num1, num2) {
+let multiply = function multiply(num1, num2) {
     return num1 * num2;
 };
 
-function divide(num1, num2) {
+let divide = function divide(num1, num2) {
     return num1 / num2;
 };
+
+const firstNumber = 5;
+const secondNumber = 3;
+const operator = add;
+
+function operate(a, action, b) {
+    return action(a, b);
+};
+
+console.log(operate(firstNumber, operator, secondNumber));
